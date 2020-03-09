33d93325ec3d0e48d30ca9d8d0148712189dd9fc
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-03-09 15:04:47 +0100

    changed search and home

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="search">
+    <label>
+      <input type="text" name="search" id="search" v-model="searchText" />
+      <button v-on:click="search">Search</button>
+    </label>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Search'
+  name: 'Search',
+  data: () => {
+    return {
+      searchText: ''
+    }
+  },
+  methods: {
+    search() {
+      this.$emit('search', this.searchText)
+    }
+  }
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <gallery />
+    <search v-on:search="search" />
+    <gallery :searchWord="searchWord" />
     <div>
       <Modal v-if="showModal"> </Modal>
     </div>
@@ -8,6 +9,7 @@
 </template>
 
 <script>
+import search from '../components/Search.vue'
 import gallery from '../components/Gallery.vue'
 import { modalInfo, setModal } from '@/modules/modal-handler.js'
 import Modal from '../components/Modal'
@@ -15,9 +17,13 @@ export default {
   name: 'Home',
   components: {
     gallery,
-    Modal
+    Modal,
+    search
   },
   methods: {
+    search(param) {
+      this.searchWord = param
+    },
     setModal(state = 0) {
       this.showModal = false
       setModal(state)
@@ -34,7 +40,8 @@ export default {
   },
   data: () => {
     return {
-      showModal: false
+      showModal: false,
+      searchWord: ''
     }
   }
 }
