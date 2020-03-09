64f9be182654b7bbc2fb2a20988e9f47b85822bf
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-09 13:38:12 +0100

    Update Gallery.vue

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="gallery"></div>
+  <div class="gallery">
+    <h1>This is the gallery</h1>
+    <img v-for="img in imagesObjs" :key="img.id" v-bind:src="img.urls.small" />
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Gallery",
-  computed: {
-    imgData() {
-      return "this is my data";
+  name: 'Gallery',
+  data: () => {
+    return {
+      imagesObjs: []
+    }
+  },
+  created() {
+    this.getData()
+  },
+  methods: {
+    getData: async function(searchWord) {
+      searchWord = 'cars'
+      const url = `https://api.unsplash.com/search/photos?client_id=hr_mePtO3L4IJ27dp7JKMA9PBadBNTaBcOVM8wjLASc&per_page=20&query=${searchWord}`
+      const responce = await fetch(url, { method: 'GET' })
+      const data = await responce.json()
+      this.imagesObjs = data.results
     }
   }
-};
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
